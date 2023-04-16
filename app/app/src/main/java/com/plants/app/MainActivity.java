984fb938292d7ec7a3e9c48a9cfcf33911de50f1
Mikhail Alekseev <mishaalekseev2004@mail.ru>
2023-04-16 12:40:40 +0300

    chore: add image loading system

--- app/app/src/main/java/com/plants/app/MainActivity.java
+++ app/app/src/main/java/com/plants/app/MainActivity.java
@@ -1,14 +1,107 @@
 package com.plants.app;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button galleryBth, captureBtn, predictBtn;
+    ImageView imageView;
+    TextView resultOut;
+    Bitmap bitmap;
+    int imageSize = 100;
+    private static final int PICK_FROM_GALLERY = 1;
+    private static final int GET_FROM_CAMERA = 2;
+    private static final int GALLERY_REQUEST_CODE = 100;
+    private static final int CAMERA_REQUEST_CODE = 200;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getPermission();
+
+        galleryBth = findViewById(R.id.galleryBtn);
+        captureBtn = findViewById(R.id.captureBtn);
+        predictBtn = findViewById(R.id.predictBtn);
+        resultOut = findViewById(R.id.resultOut);
+        imageView = findViewById(R.id.imageView);
+
+        galleryBth.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(galleryIntent, PICK_FROM_GALLERY);
+            }
+        });
+
+        captureBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(cameraIntent, GET_FROM_CAMERA);
+            }
+        });
+    }
+
+    void getPermission(){
+        if (checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.CAMERA}, CAMERA_REQUEST_CODE);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == CAMERA_REQUEST_CODE){
+            if (grantResults.length > 0 && grantResults[0] != PackageManager.PERMISSION_GRANTED){
+                this.getPermission();
+            }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            //gallery
+            case PICK_FROM_GALLERY:
+                Uri selectedImage = data.getData();
+                try {
+                    bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), selectedImage);
+                    imageView.setImageBitmap(bitmap);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                bitmap = Bitmap.createScaledBitmap(bitmap, imageSize, imageSize, false);
+                break;
+
+            //camera
+            case GET_FROM_CAMERA:
+                bitmap = (Bitmap) data.getExtras().get("data");
+                imageView.setImageBitmap(bitmap);
+                bitmap = Bitmap.createScaledBitmap(bitmap, imageSize, imageSize, false);
+                break;
+
+        }
     }
 }
