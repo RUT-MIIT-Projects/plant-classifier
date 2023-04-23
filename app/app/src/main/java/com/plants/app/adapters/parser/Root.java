96883fdb9454ef99fc5c1133707ff1ad6b031b8f
Mikhail Alekseev <mishaalekseev2004@mail.ru>
2023-04-23 14:18:33 +0300

    fix: correct class Root

--- app/app/src/main/java/com/plants/app/adapters/parser/Root.java
+++ app/app/src/main/java/com/plants/app/adapters/parser/Root.java
@@ -3,12 +3,7 @@ package com.plants.app.adapters.parser;
 import java.util.ArrayList;
 
 public class Root {
-    private String name;
     private ArrayList<Plant> plants;
-
-    public String getName() {
-        return name;
-    }
 
     public ArrayList<Plant> getPlants() {
         return plants;
