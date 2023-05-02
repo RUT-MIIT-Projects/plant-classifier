416e8f328ade2558c36021bded6262c7f77ebc53
Mikhail Alekseev <mishaalekseev2004@mail.ru>
2023-05-02 21:03:02 +0300

    feat: editing fragment design

--- app/app/src/main/java/com/plants/app/fragments/ArticleFragment.java
+++ app/app/src/main/java/com/plants/app/fragments/ArticleFragment.java
@@ -10,13 +10,12 @@ import android.view.ViewGroup;
 
 import com.plants.app.R;
 
-
-public class ProfileFragment extends Fragment {
+public class ArticleFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        return inflater.inflate(R.layout.fragment_article, container, false);
     }
 }
