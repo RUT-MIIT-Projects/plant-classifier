c8411924369ffa8b0d2f88eae327cc86003ba5b8
Mikhail Alekseev <mishaalekseev2004@mail.ru>
2023-05-21 09:48:35 +0300

    feat: remove splash screen

--- app/app/src/main/java/com/plants/app/MainActivity.java
+++ app/app/src/main/java/com/plants/app/MainActivity.java
@@ -1,7 +1,6 @@
 package com.plants.app;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.splashscreen.SplashScreen;
 import androidx.navigation.NavController;
 import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -20,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        SplashScreen.installSplashScreen(this);
+
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
